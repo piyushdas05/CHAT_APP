cb9b630dd7762d611468066cd66302d7ac110b2d
Piyush <piyushdas790@gmail.com>
2026-08-13 11:35:14 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js";
 
 
-
+const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
@@ -14,6 +17,16 @@ const app=express();
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
+}
 
 app.listen(PORT,()=>{ 
     console.log("server running on PORT")
