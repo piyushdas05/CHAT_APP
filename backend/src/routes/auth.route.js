f893edd8f19e71b53df6502fa76bc835fc89c9f2
Piyush <piyushdas790@gmail.com>
2026-08-14 17:17:53 +0530

    Rate limiting implemented.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,11 @@
 import express from "express"
 import { signup, login, logout,updateProfile } from "../controllers/auth.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router=express.Router();
+
+router.use(arcjetProtection);
+
 
 router.get("/",(req,res)=>{
     res.send("Hello user! welcoome to our chat app . it is under devolopment")
