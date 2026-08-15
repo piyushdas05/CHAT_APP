ba6388a1078fda4f36eb4880b7feba9c26c5bdea
Piyush <piyushdas790@gmail.com>
2026-08-15 15:32:00 +0530

    signup function implemented

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
+/** @type {import('tailwindcss').Config} */
 export default {
- content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        border: "border 4s linear infinite",
+      },
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
+      },
+    },
   },
-  plugins: [
-    daisyui,
-  ],
-}
+  plugins: [daisyui],
+};
 
